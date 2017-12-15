d73f22871e1ef6fa3a69280c332fcaf4b466b4b1
Sam Morrison <smorrison@wayfair.com>
2017-12-15 15:43:38 -0500

    recieve data from doodleClassifier

---
+++ server/server.js
@@ -0,0 +1,10 @@
+var osc = require('node-osc');
+
+var oscServer = new osc.Server(3333, '0.0.0.0');
+
+oscServer.on("message", function (msg, rinfo) {
+      console.log("TUIO message:");
+      console.log(msg);
+});
+
+console.log('listening started');
